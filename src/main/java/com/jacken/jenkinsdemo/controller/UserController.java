5f8bb96cf058c4a0b39233f485b3c07acd0dee5d
wangqiang <850994281@qq.com>
2019-08-10 20:59:57 +0800

    jenkins commit

---
+++ src/main/java/com/jacken/jenkinsdemo/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.jacken.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+public class UserController {
+    @RequestMapping("/user")
+    public  String getUserName(){
+        return "小明同学";
+    }
+
+}
