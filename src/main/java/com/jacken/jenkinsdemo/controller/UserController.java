bae2a3a8667b33d49eac5c7aacfe3ae04d218035
wangqiang <850994281@qq.com>
2019-08-11 16:30:59 +0800

    修改返回值

--- src/main/java/com/jacken/jenkinsdemo/controller/UserController.java
+++ src/main/java/com/jacken/jenkinsdemo/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @RequestMapping("/user")
     public  String getUserName(){
-        return "文明同学你真帅！！！！！！";
+        return "文明同学你真帅,哈哈哈哈";
     }
 
 }
