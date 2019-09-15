4825f6a283da97e3f748299bf6c7c791212e43f2
wangqiang <850994281@qq.com>
2019-09-15 19:03:25 +0800

    修改commit message

--- src/main/java/com/jacken/jenkinsdemo/controller/UserController.java
+++ src/main/java/com/jacken/jenkinsdemo/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @RequestMapping("/user")
     public  String getUserName(){
-        return "你个傻子，以后的路还很长，希望你遇到的那个人能够真心对你好，同样能给你想要的生活，要幸福哈哈哈哈哈哈";
+        return "你个傻子，以后的路还很长，希望你遇到的那个人能够真心对你好，同样能给你想要的生活，要幸福哈哈哈哈哈哈。";
     }
 
 }
