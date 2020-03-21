52f77780c0472c8019fc8ba68400349d250adb2b
王强 <wqexpore@163.com>
2020-03-21 21:22:20 +0800

    龚玲我爱你

--- src/main/java/com/jacken/jenkinsdemo/controller/UserController.java
+++ src/main/java/com/jacken/jenkinsdemo/controller/UserController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     @RequestMapping("/user")
-    public  String getUserName(){
-        return "你个傻子，以后的路还很长，希望你遇到的那个人能够真心对你好，同样能给你想要的生活，要幸福哈哈哈哈哈哈。";
+    public  String getUserName() {
+        return "龚玲 我爱你";
     }
-
 }
